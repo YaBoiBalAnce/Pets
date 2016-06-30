3e3b2bc914b12a50003ef3713f52659e772f5cd4
Pogogo007 <Francisd246@hotmail.com>
2016-06-30 15:07:04 -0400

    Update PetCommand.php

--- src/pets/command/PetCommand.php
+++ src/pets/command/PetCommand.php
@@ -134,4 +134,6 @@ class PetCommand extends PluginCommand {
 							$sender->sendMessage("/pet type [type]");
 							$sender->sendMessage("Types: blaze, pig, chicken, dog, rabbit, magma, bat, silverfish");
 						return true;
-          }
+            }
+        }
+}
