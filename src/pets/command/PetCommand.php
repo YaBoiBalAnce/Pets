1012132d45cbbe477755b0e38f10519b6a0e1f2e
Pogogo007 <Francisd246@hotmail.com>
2016-06-30 15:37:41 -0400

    Update PetCommand.php

--- src/pets/command/PetCommand.php
+++ src/pets/command/PetCommand.php
@@ -27,7 +27,7 @@ class PetCommand extends PluginCommand {
                            $sender->sendMessage(TextFormat::RED."You do not have permission to use this command");
 			}
                     return true;
-                break;
+                
 		switch (strtolower($args[0])){
 			case "name":
 			case "setname":
